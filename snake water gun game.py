490d495cd48a8d74a2c6b2c7266ed95535fad1fe
Sherry12-max <f2023266959@umt.edu.pk>
2024-10-08 23:29:26 +0500

    Snake water gun game

---
+++ snake water gun game.py
@@ -0,0 +1,22 @@
+import random
+def check(comp,user):
+    if comp==user:
+        return 0
+    if(comp==0 and user==1):
+        return -1
+    if(comp==1 and user==2):
+        return -1
+    if(comp==2 and user==0):
+        return -1
+    return 1
+comp=random.randint(0,2)
+user=int(input("0 for snake , 1 for water ,2 for gun : "))
+score=check(comp,user)
+print("COMP=" ,comp)
+print("You=" ,user)
+if(score==0):
+    print("With draw")
+elif(score==-1):
+    print("You lose")
+else:
+    print("You won")
